4f428c77025834b3884229c1bc9b4b240a5c98f4
Michel Tremblay <Michel.Tremblay@ccnb.ca>
2022-09-25 18:50:56 -0300

    Modifier la couleur de l'arrière-plan du formulaire.

--- HelloGitHubVS/HelloGitHubVSForm.Designer.cs
+++ HelloGitHubVS/HelloGitHubVSForm.Designer.cs
@@ -29,12 +29,13 @@
         private void InitializeComponent()
         {
             this.helloButton = new System.Windows.Forms.Button();
+            this.modifierButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // helloButton
             // 
             this.helloButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.helloButton.Location = new System.Drawing.Point(93, 73);
+            this.helloButton.Location = new System.Drawing.Point(99, 40);
             this.helloButton.Name = "helloButton";
             this.helloButton.Size = new System.Drawing.Size(153, 36);
             this.helloButton.TabIndex = 0;
@@ -42,11 +43,22 @@
             this.helloButton.UseVisualStyleBackColor = false;
             this.helloButton.Click += new System.EventHandler(this.helloButton_Click);
             // 
+            // modifierButton
+            // 
+            this.modifierButton.Location = new System.Drawing.Point(99, 83);
+            this.modifierButton.Name = "modifierButton";
+            this.modifierButton.Size = new System.Drawing.Size(153, 52);
+            this.modifierButton.TabIndex = 1;
+            this.modifierButton.Text = "Modifier la couleur de l\'arrière-plan du formulaire";
+            this.modifierButton.UseVisualStyleBackColor = true;
+            this.modifierButton.Click += new System.EventHandler(this.modifierButton_Click);
+            // 
             // HelloGitHubVSForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(364, 192);
+            this.Controls.Add(this.modifierButton);
             this.Controls.Add(this.helloButton);
             this.Name = "HelloGitHubVSForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -58,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Button helloButton;
+        private System.Windows.Forms.Button modifierButton;
     }
 }
 
--- HelloGitHubVS/HelloGitHubVSForm.cs
+++ HelloGitHubVS/HelloGitHubVSForm.cs
@@ -21,5 +21,10 @@ namespace HelloGitHubVS
         {
             MessageBox.Show("Hello GitHub.");
         }
+
+        private void modifierButton_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
     }
 }
